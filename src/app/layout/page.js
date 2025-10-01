e2605d06669837b249ebb5acbddd63a74bfe6dba
TeranRK <kevin.teranr@ug.edu.ec>
2025-10-01 18:42:41 -0500

    cambioproducto

--- src/app/layout/page.js
+++ src/app/layout/page.js
@@ -1,6 +1,7 @@
 'use client';
 import Link from 'next/link';
 import { useState } from 'react';
+import Image from 'next/image';
 
 // --- SE USA LA RUTA DE IMPORTACIÓN CORRECTA ---
 import { allProperties } from '../pages/tienda-gym/products';
